f686c18db56affd002b65675b6941dadedb21b37
terra456 <terra.456@yandex.ru>
2026-06-28 15:01:56 +0500

    feat(back): add putch and delite endpoints for services and clear logs

--- backend/src/api/services.ts
+++ backend/src/api/services.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { prisma } from "../lib/prisma.js";
-import { AppError } from "../utils/app-error.js";
+import { AppError, NotFoundError } from "../utils/app-error.js";
 import { validate } from "uuid";
 import { scheduleCronForUrl } from "../lib/scaner.js";
+import { validateServiceData } from "../utils/validate-service-data.js";
 
 const router = express.Router();
 
@@ -23,8 +24,7 @@ router.post('/', async (req, res) => {
 
   scheduleCronForUrl(service);
 
-  res.status(202);
-  res.send(service);
+  res.status(202).send(service);
 });
 
 router.get('/', async (req, res) => {
@@ -32,13 +32,89 @@ router.get('/', async (req, res) => {
   res.send(services);
 });
 
-router.get('/:id/logs', async (req, res) => {
-  const path = req.url;
-  const id = path.split('/')[1];
-  console.log(path, id);
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
   if (id && validate(id)) {
-    const logs = await prisma.log.findMany({where: {serviceId: id}});
+    try {
+      const service = await prisma.service.findFirst({
+        where: { id },
+      });
+      if (!service) {
+        throw new NotFoundError(`Service with id ${id} not found`);
+      }
+      res.send(service);
+    } catch (error) {
+      throw new Error('somthing wrong');
+    }
+  } else {
+    throw new AppError('id incorrect', 400);
+  }
+});
+
+router.patch('/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+
+  if (!(id && validate(id))) {
+    throw new AppError('id incorrect', 400);
+  }
+
+  try {
+    const validData = validateServiceData(data);
+    const service = await prisma.service.update({
+      where: { id },
+      data: validData,
+    });
+    if (!service) {
+      throw new NotFoundError(`Service with id ${id} not found`);
+    }
+    res.status(202).send(service);
+  } catch (error) {
+    if (error instanceof AppError) {
+      throw error;
+    }
+    throw new Error('somthing wrong');
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  if (id && validate(id)) {
+    try {
+      const service = await prisma.service.delete({
+        where: { id },
+      });
+      if (!service) {
+        throw new NotFoundError(`Service with id ${id} not found`);
+      }
+      res.status(204).send(service);
+    } catch (error) {
+      throw new Error('somthing wrong');
+    }
+  } else {
+    throw new AppError('id incorrect', 400);
+  }
+});
+
+router.get('/:id/logs', async (req, res) => {
+  const { id } = req.params;
+  if (id && validate(id)) {
+    const logs = await prisma.log.findMany({
+      where: {serviceId: id},
+      orderBy: { createdAt: "desc" },
+      take: 100,
+    });
     res.send(logs);
+  } else {
+    throw new AppError('id incorrect', 400);
+  }
+});
+
+router.delete('/:id/logs', async (req, res) => {
+  const { id } = req.params;
+  if (id && validate(id)) {
+    const logs = await prisma.log.deleteMany({where: {serviceId: id}});
+    res.status(204).send(logs);
   } else {
     throw new AppError('id incorrect', 400);
   }
