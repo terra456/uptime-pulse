6ee895e7c0d18f75cc786349eab3fe412d7be1ec
terra456 <terra.456@yandex.ru>
2026-06-29 22:38:22 +0500

    feat(front): update form, add new fields

--- frontend/src/components/add-server-form.tsx
+++ frontend/src/components/add-server-form.tsx
@@ -3,6 +3,7 @@ import type { ServerData, ServerFormFields } from "@/types/types";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useAddNewServerMutation, useEditServerMutation } from "@/services/api";
+import { Switch } from "./ui/switch";
 
 interface AddServerFormProps {
   initialData: ServerData | null;
@@ -13,7 +14,8 @@ export function AddServerForm({ initialData, onClose }: AddServerFormProps) {
   const [formData, setFormData] = useState<ServerFormFields>({
     name: "",
     url: "",
-    interval: "5", // значение по умолчанию
+    interval: "1", // значение по умолчанию
+    isActive: true,
   });
 
   const [addNewServer, { isLoading }] = useAddNewServerMutation();
@@ -24,11 +26,17 @@ export function AddServerForm({ initialData, onClose }: AddServerFormProps) {
       setFormData({
         name: initialData.name,
         url: initialData.url,
-        interval: "5", // Если в данных сервера есть интервал, берем его
+        interval: (initialData.interval / 60).toString(), // Если в данных сервера есть интервал, берем его
+        isActive: initialData.isActive,
       });
     } else {
       // Если это создание — сбрасываем в дефолт
-      setFormData({ name: "", url: "", interval: "5" });
+      setFormData({
+        name: "",
+        url: "",
+        interval: "1",
+        isActive: true,
+      });
     }
   }, [initialData]);
 
@@ -42,13 +50,17 @@ export function AddServerForm({ initialData, onClose }: AddServerFormProps) {
     }));
   };
 
-  const handleSubmit = (e: React.SubmitEvent) => {
+  const handleSwitchChange = (checked: boolean) => {
+    setFormData((prev) => ({ ...prev, isActive: checked }));
+  };
+
+  const handleSubmit = async (e: React.SubmitEvent) => {
     e.preventDefault();
     console.log("Отправляем данные формы:", formData);
     try {
       initialData
-        ? updateServer({ id: initialData.id, ...formData })
-        : addNewServer(formData);
+        ? await updateServer({ id: initialData.id, ...formData }).unwrap()
+        : await addNewServer(formData).unwrap();
     } catch (e) {
       console.error("Failed to save the server: ");
     } finally {
@@ -76,6 +88,37 @@ export function AddServerForm({ initialData, onClose }: AddServerFormProps) {
           placeholder="https://example.com"
           value={formData.url}
           onChange={handleChange}
+        />
+      </div>
+      {/* ИНПУТ ДЛЯ ИНТЕРВАЛА СКАНИРОВАНИЯ */}
+      <div className="flex flex-col gap-1.5 text-left">
+        <div className="flex justify-between items-center">
+          <label className="text-sm font-medium">Интервал проверки</label>
+          <span className="text-xs text-muted-foreground">в минутах</span>
+        </div>
+        <Input
+          type="number" // Включает стрелочки больше/меньше в браузере
+          name="interval"
+          min={1} // Ограничение: не чаще, чем раз в 1минуту
+          max={24 * 60} // Ограничение: не реже, чем раз в сутки
+          value={formData.interval} // Избавляемся от лишних нулей при очистке поля
+          onChange={handleChange} // Используем числовой обработчик
+        />
+        <p className="text-[11px] text-muted-foreground">
+          Рекомендуется указывать значения от 1 до 60 минут.
+        </p>
+      </div>
+      <div className="flex items-center gap-5 rounded-lg border border-border p-3 bg-muted/20">
+        <label
+          htmlFor="activation-toggle"
+          className="text-sm font-medium text-foreground"
+        >
+          Запустить сканирование
+        </label>
+        <Switch
+          id="notify-toggle"
+          checked={formData.isActive}
+          onCheckedChange={handleSwitchChange}
         />
       </div>
       <div className="flex justify-end gap-2 pt-2">
